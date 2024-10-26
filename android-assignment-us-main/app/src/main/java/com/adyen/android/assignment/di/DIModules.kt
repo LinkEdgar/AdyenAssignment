b500bbc6b02afd07ac35904d910747d587b2ac6f
Edgar Link <bigfathog@Eds-MacBook-Pro.local>
2024-10-26 17:09:57 -0500

    Added koing dependency

---
+++ android-assignment-us-main/app/src/main/java/com/adyen/android/assignment/di/DIModules.kt
@@ -0,0 +1,8 @@
+package com.adyen.android.assignment.di
+
+import org.koin.dsl.module
+
+
+val planetaryModules = module {
+
+}
---
+++ android-assignment-us-main/app/src/main/java/com/adyen/android/assignment/ui/App.kt
@@ -0,0 +1,19 @@
+package com.adyen.android.assignment.ui
+
+import android.app.Application
+import com.adyen.android.assignment.di.planetaryModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(planetaryModules)
+        }
+    }
+
+}
